2a61a6d432508eabb22edb07df20c3da5c6cf1a8
Niklas <niklas.frank@gmail.com>
2015-03-06 10:16:47 +0100

    add: Angular.js to template

--- StretchGarageWeb/App_Start/BundleConfig.cs
+++ StretchGarageWeb/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace StretchGarageWeb
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
