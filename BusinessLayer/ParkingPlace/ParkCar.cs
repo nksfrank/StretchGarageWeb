7cafd2cd00801d1218c6aed8d16305d57e586612
Niklas <niklas.frank@gmail.com>
2015-03-06 11:13:29 +0100

    add: Refrences between layers add: ParkingManager

---
+++ BusinessLayer/ParkingPlace/ParkCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ParkingPlace
+{
+    public class ParkCar
+    {
+        public bool Add(Unit car, ParkingPlace parkingPlace)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
