4c5bab6a84b41e3131742c07408a8d8ebff75188
Niklas <niklas.frank@gmail.com>
2015-03-11 13:42:51 +0100

    add: Test

--- StretchGarageWeb/Controllers/WebApiControllers/ParkedCarsController.cs
+++ StretchGarageWeb/Controllers/WebApiControllers/ParkedCarsController.cs
@@ -7,11 +7,18 @@ using System.Web.Http;
 
 namespace StretchGarageWeb.Controllers.WebApiControllers
 {
+    public class Car {
+        public bool IsAvailable { get; set; }
+        public string Status{ get; set; }
+        public string CssClass { get; set; }
+    }
     public class ParkedCarsController : ApiController
     {
+        
         // GET api/ParkedCars 
         public IEnumerable<string> Get()
         {
+            BusinessLayer.ParkingManager.ParkingPlaceManager.AvaiableSpaces(0);
             return new string[] { "value1", "value2" };
         }
 
