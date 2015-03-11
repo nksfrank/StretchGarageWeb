3b2064332c6e58dd6988a7c69a79c0bc978141cf
johntakter <john.takter@gmail.com>
2015-03-11 16:39:55 +0100

    Added ParkedCar class. Used as return object in ApiResponse for webpage api request

---
+++ Objects/WebApiResponse/ParkedCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects.WebApiResponse
+{
+    public class ParkedCar
+    {
+        public ParkedCar(bool isAvailable, string status, string cssClass)
+        {
+            IsAvailable = isAvailable;
+            Status = status;
+            CssClass = cssClass;
+        }
+        public bool IsAvailable { get; set; }
+        public string Status{ get; set; }
+        public string CssClass { get; set; }
+    }
+}
