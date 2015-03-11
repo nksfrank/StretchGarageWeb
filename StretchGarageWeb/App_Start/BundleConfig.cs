1671ec7249f27d358bb9c0ebfb8dd0a283817d25
Niklas <niklas.frank@gmail.com>
2015-03-11 10:09:08 +0100

    delete: Unnecessary javascript files add: Angular Animate

--- StretchGarageWeb/App_Start/BundleConfig.cs
+++ StretchGarageWeb/App_Start/BundleConfig.cs
@@ -12,18 +12,11 @@ namespace StretchGarageWeb
                         "~/Scripts/garage.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/Scripts/angular.min.js"));
+                        "~/Scripts/angular.min.js",
+                        "~/Scripts/angular-animate.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-1.10.2.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js",
