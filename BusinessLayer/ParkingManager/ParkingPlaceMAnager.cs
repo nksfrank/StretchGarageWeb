c8f85a3376c6bc50a4d30aa2bfe7bcf6bb3a9bfd
Niklas <niklas.frank@gmail.com>
2015-03-11 16:49:00 +0100

    add: Get Method for fetching parking results for frontpage

--- BusinessLayer/ParkingManager/ParkingPlaceMAnager.cs
+++ BusinessLayer/ParkingManager/ParkingPlaceMAnager.cs
@@ -12,6 +12,42 @@ namespace BusinessLayer.ParkingManager
 {
     public class ParkingPlaceManager : MainHandler
     {
+        internal class FrontPageResponse
+        {
+            public bool IsAvailable {get; set;}
+            public string CssClass { get; set; }
+            public string Status { get; set; }
+        }
+        public static IError Get(int ParkingPlaceID)
+        {
+            if (!DB.ParkingPlaces.Any(a => a.Id == ParkingPlaceID))
+                return new Error { 
+                    Message = "There is no parking place with id " + ParkingPlaceID,
+                    Success = false
+                };
+
+            var parking = DB.ParkingPlaces.Where(a => a.Id == ParkingPlaceID).Select(a => new {spots = a.ParkingSpots, numOfParkedCars = a.ParkedCars.Count(b => b.IsParked == true && b.ParkingDate.Date == DateTime.Now.Date) }).FirstOrDefault();
+
+            var cars = new List<FrontPageResponse>();
+            for (int i = 0; i < parking.spots; i++)
+            {
+                var car = i < parking.numOfParkedCars
+                    ? new FrontPageResponse {
+                        IsAvailable = false,
+                        CssClass = "red",
+                        Status = "Occupied",
+                    }
+                    : new FrontPageResponse {
+                        IsAvailable = false,
+                        CssClass = "red",
+                        Status = "Occupied",
+                    };
+                cars.Add(car);
+            }
+
+            throw new NotImplementedException();
+        }
+
         public static IQueryable AvaiableSpaces(int parkingPlaceID)
         {
             return DB.ParkingPlaces.Where(a => a.Id == parkingPlaceID).Select(a => new { spots = a.ParkingSpots, parked = a.ParkedCars.Where(b => b.IsParked == true)});
@@ -21,5 +57,7 @@ namespace BusinessLayer.ParkingManager
         {
             return DB.ParkingPlaces.First(a => a.Id == parkingPlace.Id).ParkingSpots;
         }
+
+
     }
 }
