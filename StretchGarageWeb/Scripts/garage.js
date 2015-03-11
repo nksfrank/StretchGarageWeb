bdd673622345920a0beab7905bba3b3d79f46ab0
Niklas <niklas.frank@gmail.com>
2015-03-11 13:43:09 +0100

    add: Javascript maps

--- StretchGarageWeb/Scripts/garage.js
+++ StretchGarageWeb/Scripts/garage.js
@@ -1,4 +1,49 @@
-﻿$(document).ready(
+﻿var garageApp = angular.module("GarageApp", [])
+
+.controller("AppController", ['$scope', '$interval', '$http', '$timeout', '$q',
+    function ($scope, $interval, $http, $timeout, $q) {
+
+        var fetchRequest = null;
+        $interval(function () {
+            if (fetchRequest) { fetchRequest.resolve(); }
+            fetchRequest = $q.defer();
+
+            $scope.Messages = [{ Message: "Uppdaterar platser från servern" }, ];
+            $("#message").slideDown("slow");
+            $http({
+                method: 'GET',
+                url: 'http://nikz.se/projects/angular/garage/mockup.php',
+            }).success(function (data) {
+                $timeout(function () {
+                    $scope.Messages = {};
+                    $("#message").slideUp("slow");
+                    //Move out of timer for live update
+                    $scope.Spots = data;
+                }, 2000);
+            }).error(function () {
+                $scope.Message = "Error";
+                $("#message").slideDown("slow");
+            });
+        }, 7000);
+
+        $scope.close = function (id) {
+            $("#" + id).slideUp();
+        };
+
+        $scope.Messages;
+        $scope.Spots = [
+            { IsAvailable: false, Status: "UPPTAGEN", CssClass: "red" },
+            { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
+            { IsAvailable: false, Status: "UPPTAGEN", CssClass: "red" },
+            { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
+            { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
+            { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
+            { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
+            { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
+        ];
+    }]);
+
+$(document).ready(
 
 
 
