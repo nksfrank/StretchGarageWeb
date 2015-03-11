c40b9d6a0107e6bab41f2f330232ce05cee2b364
Niklas <niklas.frank@gmail.com>
2015-03-11 14:59:16 +0100

    add: ShowMessage Method

--- StretchGarageWeb/Scripts/garage.js
+++ StretchGarageWeb/Scripts/garage.js
@@ -2,35 +2,39 @@
 
 .controller("AppController", ['$scope', '$interval', '$http', '$timeout', '$q',
     function ($scope, $interval, $http, $timeout, $q) {
+        $scope.ParkingId = 0;
 
         var fetchRequest = null;
         $interval(function () {
             if (fetchRequest) { fetchRequest.resolve(); }
             fetchRequest = $q.defer();
 
-            $scope.Messages = [{ Message: "Uppdaterar platser från servern" }, ];
-            $("#message").slideDown("slow");
+            $scope.ShowMessage("Uppdaterar platser från servern");
+
             $http({
                 method: 'GET',
-                url: 'http://nikz.se/projects/angular/garage/mockup.php',
-            }).success(function (data) {
-                $timeout(function () {
-                    $scope.Messages = {};
-                    $("#message").slideUp("slow");
-                    //Move out of timer for live update
-                    $scope.Spots = data;
-                }, 2000);
-            }).error(function () {
-                $scope.Message = "Error";
-                $("#message").slideDown("slow");
+                url: '/api/ParkingSpot/',
+                parameter: $scope.ParkingId,
+            }).success(function(data) {    
+                $scope.Spots = data;
+            }).error(function() {
+                $scope.ShowMessage("Error");
             });
         }, 7000);
 
-        $scope.close = function (id) {
+        $scope.ShowMessage = function(msg) {
+            $scope.Messages = msg;
+            $("#message").slideDown(400);
+            $timeout(function() {
+                $scope.Messages = {};
+                $("#message").slideUp(300);
+            }, 2000);
+        };
+
+        $scope.close = function(id) {
             $("#" + id).slideUp();
         };
 
-        $scope.Messages;
         $scope.Spots = [
             { IsAvailable: false, Status: "UPPTAGEN", CssClass: "red" },
             { IsAvailable: true, Status: "LEDIG", CssClass: "green" },
