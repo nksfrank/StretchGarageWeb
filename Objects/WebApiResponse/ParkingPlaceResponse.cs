816bcd4deea4637a9d53d31370e1e57cd8cb823a
Niklas <niklas.frank@gmail.com>
2015-03-13 09:06:21 +0100

    Response item for parkingplace list

---
+++ Objects/WebApiResponse/ParkingPlaceResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects.WebApiResponse
+{
+    public class ParkingPlaceResponse
+    {
+        public string Name { get; set; }
+        public string CssClass { get; set; }
+        public int ParkingSpots { get; set; }
+    }
+}
