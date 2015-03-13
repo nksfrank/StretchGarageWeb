daaf027d99dcbdd0a81046bd50457792e471a851
Niklas <niklas.frank@gmail.com>
2015-03-13 08:37:25 +0100

    Controller for managing ParkingPlace requests

---
+++ StretchGarageWeb/Controllers/WebApiControllers/ParkingPlaceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Objects.WebApiResponse;
+using BusinessLayer.ParkingManager;
+
+namespace StretchGarageWeb.Controllers.WebApiControllers
+{
+    public class ParkingPlaceController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            ParkingPlaceManager.GetAllParkingPlaces(id);
+            var result = new ApiResponse();
+            return Request.CreateResponse<ApiResponse>(HttpStatusCode.OK, result);
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "");
+        }
+
+        // PUT api/<controller>/5
+        public HttpResponseMessage Put(int id, [FromBody]string value)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "");
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(int id)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "");
+        }
+    }
+}
