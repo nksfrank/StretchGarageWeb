80fea42fa432f13e575a15aeb7dad4afe8deeba5
helium45 <gustav.hedman@gmail.com>
2015-03-13 13:17:42 +0100

    Bytt namn på Get till GetParkedCars och flyttat metoden från ParkingPlaceManager till ParkCarManager

--- BusinessLayer/ParkingManager/ParkCarManager.cs
+++ BusinessLayer/ParkingManager/ParkCarManager.cs
@@ -58,6 +58,29 @@ namespace BusinessLayer.ParkingManager
             return new Error() { Success = true, Message = "" };
         }
 
+        public static IError GetParkedCars(int ParkingPlaceID)
+        {
+            if (!DB.ParkingPlaces.Any(a => a.Id == ParkingPlaceID))
+                return new Error
+                {
+                    Message = "There is no parking place with id " + ParkingPlaceID,
+                    Success = false
+                };
+
+            var parking = DB.ParkingPlaces.Where(a => a.Id == ParkingPlaceID).Select(a => new { spots = a.ParkingSpots, numOfParkedCars = a.ParkedCars.Count(b => b.IsParked == true && b.ParkingDate.Date == DateTime.Now.Date) }).FirstOrDefault();
+
+            var cars = new List<Objects.WebApiResponse.ParkedCarResponse>();
+            for (int i = 0; i < parking.spots; i++)
+            {
+                var car = i < parking.numOfParkedCars
+                    ? new Objects.WebApiResponse.ParkedCarResponse(false, "Occupied", "red")
+                    : new Objects.WebApiResponse.ParkedCarResponse(false, "Vaccant", "green");
+                cars.Add(car);
+            }
+
+            return new Objects.WebApiResponse.ApiResponse(true, "", cars);
+        }
+
         public bool IsParked(Unit car)
         {
             return DB.ParkedCars.Where(a => a.UnitId == car.Id).Any(a => a.IsParked);
--- BusinessLayer/ParkingManager/ParkingPlaceMAnager.cs
+++ BusinessLayer/ParkingManager/ParkingPlaceMAnager.cs
@@ -12,29 +12,7 @@ using Objects.WebApiResponse;
 namespace BusinessLayer.ParkingManager
 {
     public class ParkingPlaceManager : MainHandler
-    {
-        public static IError Get(int ParkingPlaceID)
-        {
-            if (!DB.ParkingPlaces.Any(a => a.Id == ParkingPlaceID))
-                return new Error { 
-                    Message = "There is no parking place with id " + ParkingPlaceID,
-                    Success = false
-                };
-
-            var parking = DB.ParkingPlaces.Where(a => a.Id == ParkingPlaceID).Select(a => new {spots = a.ParkingSpots, numOfParkedCars = a.ParkedCars.Count(b => b.IsParked == true && b.ParkingDate.Date == DateTime.Now.Date) }).FirstOrDefault();
-
-            var cars = new List<Objects.WebApiResponse.ParkedCarResponse>();
-            for (int i = 0; i < parking.spots; i++)
-            {
-                var car = i < parking.numOfParkedCars
-                    ? new Objects.WebApiResponse.ParkedCarResponse(false, "Occupied", "red")
-                    : new Objects.WebApiResponse.ParkedCarResponse(false, "Vaccant", "green");
-                cars.Add(car);
-            }
-
-            return new Objects.WebApiResponse.ApiResponse(true, "", cars);
-        }
-
+    {        
         public static IError GetAllParkingPlaces()
         {
             if(DB.ParkingPlaces.Count() <= 0) {
@@ -56,7 +34,5 @@ namespace BusinessLayer.ParkingManager
         {
             return DB.ParkingPlaces.First(a => a.Id == parkingPlaceId).ParkingSpots;
         }
-
-
     }
 }
