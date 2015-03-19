78dbefbe5da93a1b96d4312c398e209b76563d7e
Niklas <niklas.frank@gmail.com>
2015-03-19 15:49:44 +0100

    Distance Calculations between Car and ParkingPlace

--- BusinessLayer/LocationManager/LocationManager.cs
+++ BusinessLayer/LocationManager/LocationManager.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.Database;
 
 namespace BusinessLayer.LocationManager
 {
     public class LocationManager : MainHandler
     {
-        public static double CalculateDistanceToParkingPlace()
+        public static int GetClosestParkingPlaceId(double carLat, double carLong)
         {
-            throw new NotImplementedException();
+            var parkingPlaces = DB.ParkingPlaces.Select(a => new {a.Id, a.Lat, a.Long}).ToList();
+
+            var closest = double.MaxValue;
+            var parkingPlaceId = -1;
+            foreach (var p in parkingPlaces)
+            {
+                var dist = Utilities.Gps.DistanceBetweenPlacesM(carLat, carLong, (double)p.Lat, (double)p.Long);
+
+                if (!(dist < closest)) continue;
+
+                closest = dist;
+                parkingPlaceId = p.Id;
+            }
+            return parkingPlaceId;
+        }
+
+        public static double GetDistanceToParkingPlace(int parkingPlaceId, double carLat, double carLong)
+        {
+            var p = DB.ParkingPlaces.Where(a => a.Id == parkingPlaceId).Select(a => new {a.Lat, a.Long}).First();
+            return Utilities.Gps.DistanceBetweenPlacesM(carLat, carLong, (double)p.Lat, (double)p.Long);
         }
     }
 }
