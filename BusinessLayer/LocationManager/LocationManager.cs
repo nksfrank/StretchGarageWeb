2532ff24f067504d053d1a1567d58e596f9ae3ec
Niklas <niklas.frank@gmail.com>
2015-03-19 14:42:38 +0100

    Location methods

---
+++ BusinessLayer/LocationManager/LocationManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.LocationManager
+{
+    public class LocationManager : MainHandler
+    {
+        public static double CalculateDistanceToParkingPlace()
+        {
+        }
+    }
+}
