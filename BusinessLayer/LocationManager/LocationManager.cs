e314aaa9827973cd2f0aa3be39416928c56d5176
Niklas <niklas.frank@gmail.com>
2015-03-19 16:12:28 +0100

    Process Location Request now parks car if close to a parkingplace

--- BusinessLayer/LocationManager/LocationManager.cs
+++ BusinessLayer/LocationManager/LocationManager.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using DataLayer.Database;
+using Objects;
+using Objects.Interface;
 
 namespace BusinessLayer.LocationManager
 {
     public class LocationManager : MainHandler
     {
+        public static IError ProcessLocationRequest(int carId, double carLat, double carLong)
+        {
+            var pId = GetClosestParkingPlaceId(carLat, carLong);
+            var dist = GetDistanceToParkingPlace(carLat, carLong, pId);
+
+            if (dist < 20)
+            {
+                ParkedCar car = new ParkedCar {UnitId = carId, ParkingPlaceId = pId};
+                DB.ParkedCars.InsertOnSubmit(car);
+
+                try
+                {
+                    DB.SubmitChanges();
+                }
+                catch (Exception)
+                {
+                    return new Error { Message = "Kunde inte parkera bilen", Success = false };
+                }
+            }
+            throw new NotImplementedException();
+        }
         public static int GetClosestParkingPlaceId(double carLat, double carLong)
         {
             var parkingPlaces = DB.ParkingPlaces.Select(a => new {a.Id, a.Lat, a.Long}).ToList();
@@ -18,7 +40,7 @@ namespace BusinessLayer.LocationManager
             var parkingPlaceId = -1;
             foreach (var p in parkingPlaces)
             {
-                var dist = Utilities.Gps.DistanceBetweenPlacesM(carLat, carLong, (double)p.Lat, (double)p.Long);
+                var dist = GetDistanceToParkingPlace(carLat, carLong, (double)p.Lat, (double)p.Long);
 
                 if (!(dist < closest)) continue;
 
@@ -28,10 +50,15 @@ namespace BusinessLayer.LocationManager
             return parkingPlaceId;
         }
 
-        public static double GetDistanceToParkingPlace(int parkingPlaceId, double carLat, double carLong)
+        public static double GetDistanceToParkingPlace(double carLat, double carLong, int parkingPlaceId)
         {
             var p = DB.ParkingPlaces.Where(a => a.Id == parkingPlaceId).Select(a => new {a.Lat, a.Long}).First();
-            return Utilities.Gps.DistanceBetweenPlacesM(carLat, carLong, (double)p.Lat, (double)p.Long);
+            return GetDistanceToParkingPlace(carLat, carLong, (double)p.Lat, (double)p.Long);
+        }
+
+        private static double GetDistanceToParkingPlace(double carLat, double carLong, double pLat, double pLong)
+        {
+            return Utilities.Gps.DistanceBetweenPlacesM(carLat, carLong, pLat, pLong);
         }
     }
 }
