f80e57a9ef491684cd5ccce88ed82142347de660
Niklas <niklas.frank@gmail.com>
2015-03-19 10:57:31 +0100

    Added DataContract to objects for serialization

--- Objects/WebApiResponse/ParkedCarResponse.cs
+++ Objects/WebApiResponse/ParkedCarResponse.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Objects.WebApiResponse
 {
+    [DataContract]
     public class ParkedCarResponse
     {
+        public ParkedCarResponse() {}
         public ParkedCarResponse(bool isAvailable, string status, string cssClass)
         {
             IsAvailable = isAvailable;
             Status = status;
             CssClass = cssClass;
         }
+        [DataMember]
         public bool IsAvailable { get; set; }
+        [DataMember]
         public string Status{ get; set; }
+        [DataMember]
         public string CssClass { get; set; }
     }
 }
