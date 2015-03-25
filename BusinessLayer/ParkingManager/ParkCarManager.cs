a782938cc2e7468e8b5402bfa369cabd8ddfaf34
Niklas <niklas.frank@gmail.com>
2015-03-25 11:34:26 +0100

    Removed unnessessary usings

--- BusinessLayer/ParkingManager/ParkCarManager.cs
+++ BusinessLayer/ParkingManager/ParkCarManager.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataLayer;
 using DataLayer.Database;
 using Objects;
 using Objects.Interface;
