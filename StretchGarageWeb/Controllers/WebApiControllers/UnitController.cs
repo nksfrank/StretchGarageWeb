09961b545d63001f5f97773ea558f9564f8ff7da
nksfrank <niklas.frank@gmail.com>
2015-04-24 11:18:28 +0200

    Made use of unittype

--- StretchGarageWeb/Controllers/WebApiControllers/UnitController.cs
+++ StretchGarageWeb/Controllers/WebApiControllers/UnitController.cs
@@ -14,7 +14,8 @@ namespace StretchGarageWeb.Controllers
         // GET api/CreateUnit/5
         public HttpResponseMessage Get(string username, int type)
         {
-            var res = BusinessLayer.UnitMgr.UnitManager.CreateUnit(username, type);
+            UnitType unitType = (UnitType)type;
+            var res = BusinessLayer.UnitMgr.UnitManager.CreateUnit(username, unitType);
             if (res is Error)
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, res.Message);
 
